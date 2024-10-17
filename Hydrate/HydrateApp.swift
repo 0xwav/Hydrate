c644b92007a0cec0be24990325bc0624299360e3
0xwav <raghadmohammed553@gmail.com>
2024-10-17 10:49:12 +0300

    Initial Commit

---
+++ Hydrate/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate
+//
+//  Created by Raghad on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
